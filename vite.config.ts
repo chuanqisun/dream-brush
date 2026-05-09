09b64f3df06b45f9d0432f82331fb659683886ec
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2026-05-09 13:30:01 -0400

    migrate repo

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,5 @@
-import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        pipeline: resolve(__dirname, "107-pipeline.html"),
-      },
-    },
-  },
-  base: "/ink-form/",
+  base: "/dream-brush/",
 });
